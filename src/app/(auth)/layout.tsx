457bf5a6ad1ba056f36aa6f08b11d7ce13713d35
WebApp07 <aminezouguig@proton.me>
2024-07-12 14:08:07 +0100

    Dark mode username auth

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,9 @@
+import { Logo } from "./_components/logo";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex items-center justify-center bg-gradient-to-l from-rose-400 via-fuchsia-500 to-indigo-500		">
+    <div className="h-full flex flex-col items-center justify-center bg-gradient-to-l from-rose-400 via-fuchsia-500 to-indigo-500 space-y-6">
+      <Logo />
       {children}
     </div>
   );
