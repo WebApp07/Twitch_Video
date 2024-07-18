2673d2c5481fa82d04aaa64b50bc59bed96baea5
WebApp07 <aminezouguig@proton.me>
2024-07-18 16:37:02 +0100

    Recommended list

--- store/use-sidebar.ts
+++ store/use-sidebar.ts
@@ -8,6 +8,6 @@ interface SidebarStore {
 
 export const useSidebar = create<SidebarStore>((set) => ({
   collapsed: false,
-  onExpand: () => set({ collapsed: false }),
-  onCollapse: () => set({ collapsed: true }),
+  onExpand: () => set(() => ({ collapsed: false })),
+  onCollapse: () => set(() => ({ collapsed: true })),
 }));
