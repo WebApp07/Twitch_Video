f76306173aa872c51a99d8a05079b5b866be61e8
WebApp07 <aminezouguig@proton.me>
2024-07-22 10:31:26 +0100

    Create model stream and fixe some problems about prisma

---
+++ src/app/(dashboard)/u/[username]/chat/page.tsx
@@ -0,0 +1,9 @@
+const ChatPage = () => {
+  return (
+    <div className="p-6">
+      <div className="mb-4">
+        <h1 className="text-2xl font-bold">Chat settings</h1>
+      </div>
+    </div>
+  );
+};
