5f6e3ef97fea1632fa9526ebbe03407d8a3faecf
WebApp07 <aminezouguig@proton.me>
2024-08-05 02:23:14 +0100

    User page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import { Logo } from "./_components/logo";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex flex-col items-center justify-center bg-gradient-to-l from-rose-400 via-fuchsia-500 to-indigo-500 space-y-6">
+    <div className="h-full flex flex-col items-center justify-center space-y-6">
       <Logo />
       {children}
     </div>
