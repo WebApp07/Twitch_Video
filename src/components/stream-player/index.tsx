7a0497b939fb10d61cb15dd08a626176e681f7a8
WebApp07 <aminezouguig@proton.me>
2024-08-05 02:12:22 +0100

    about card

--- src/components/stream-player/index.tsx
+++ src/components/stream-player/index.tsx
@@ -10,6 +10,7 @@ import { Chat, ChatSkeleton } from "./chat";
 import { ChatToggle } from "./chat-toggle";
 import { Header, HeaderSkeleton } from "./header";
 import { InfoCard } from "./info-card";
+import { AboutCard } from "./about-card";
 
 type CustomStream = {
   id: string;
@@ -76,6 +77,14 @@ export const StreamPlayer = ({
             viewerIdentity={identity}
             name={stream.name}
             thumbnailUrl={stream.thumbnailUrl}
+          />
+
+          <AboutCard
+            hostName={user.username}
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            bio={user.bio}
+            followedByCount={user._count.followedBy}
           />
         </div>
         <div className={cn("col-span-1", collapsed && "hidden")}>
